70f0ac35ccb82b177d2baf38cf39ad1c8db74e43
usman <builtbyusman@gmail.com>
2026-02-14 23:53:22 +0500

    update database to supabase

--- app/db/database.py
+++ app/db/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 from sqlalchemy.ext.declarative import declarative_base
 
 
-DATABASE_URL="postgresql://usman:ShaniMalik321@localhost:5432/smart_campus_management_system"
+DATABASE_URL = "postgresql+psycopg2://postgres.spcohlicnsroxcfieipi:Shani.Malik321@aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres?sslmode=require"
 
-engine=create_engine(DATABASE_URL)
+engine=create_engine(DATABASE_URL,poolclass=NullPool)
 
 session_local=sessionmaker(autoflush=False,autocommit=False,bind=engine)
 
