3356866ecddabfd7f09c0a8e03b5fb7e81b1b119
Muhammd Usman <builtbyusman@gmail.com>
2026-02-17 17:32:24 +0800

    Add fileurl datatype in submission schema

--- app/schemas/submission.py
+++ app/schemas/submission.py
@@ -1,16 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,FileUrl
 from typing import Optional
 
 
 class SubmissionCreate(BaseModel):
     assignment_id:int
-    file_url:str
+    file_url:FileUrl
 
 class SubmissionResponse(BaseModel):
     id:int
     assignment_id:int
     student_id:int
-    file_url:str
+    file_url:FileUrl
     grade:Optional[float]
 
     class Config:
