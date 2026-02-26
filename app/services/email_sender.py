dc296fbc355fb2a8eb1214789a91bd3316d3a1ea
Muhammd Usman <builtbyusman@gmail.com>
2026-02-26 17:15:02 +0500

    Fully updated

--- app/services/email_sender.py
+++ app/services/email_sender.py
@@ -19,31 +19,3 @@ def send_email_otp(to_email: str, otp: str):
     })
 
 
-
-
-
-
-# sender_email = os.getenv("EMAIL")
-#     app_password = os.getenv("EMAIL_APP_PASSWORD")
-
-#     message = MIMEMultipart("alternative")
-#     message["Subject"] = "Smart Campus OTP Verification"
-#     message["From"] = sender_email
-#     message["To"] = to_email
-
-#     html_content = f"""
-#     <h2>Your OTP Code</h2>
-#     <h1>{otp}</h1>
-#     <p>This OTP is valid for 3 minutes.</p>
-#     """
-
-#     message.attach(MIMEText(html_content, "html"))
-
-#     with smtplib.SMTP("smtp.gmail.com", 587) as server:
-#         server.starttls()
-#         server.login(sender_email, app_password)
-#         server.sendmail(sender_email, to_email, message.as_string())
-
-
-
-
